800a9ff916519d94fb2875f82182d0d1bff2a4b7
Gansta-Girs <girs1982@mail.ru>
2021-07-28 16:38:26 +0300

    Add files via upload

---
+++ shlak.h
@@ -0,0 +1,10 @@
+#ifndef SHLAK_H 
+#define SHLAK_H 
+#define rxPin 4   ////pin reciver
+#include <Arduino.h>
+#define MAX_DELTA_1 200
+#define rxPin 4 
+void grabshlak();
+String code_shlak();
+
+#endif
